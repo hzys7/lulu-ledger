9d23cbcdfe72ead7302ffa057541382a3eb80c7a
Lulu Ledger <gkk953579@users.noreply.github.com>
2026-06-14 00:06:14 +0800

    fix: import Keyboard in AddTransactionScreen (crash on submit note)

--- src/screens/AddTransactionScreen.js
+++ src/screens/AddTransactionScreen.js
@@ -9,6 +9,7 @@ import {
   TextInput,
   Alert,
   Platform,
+  Keyboard,
 } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { Ionicons } from '@expo/vector-icons';
